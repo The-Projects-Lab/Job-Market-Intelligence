41fe88cb80dbbec4e93a7b9efff6c9fa41e480a0
TusharRaut01 <tusharrajeshraut03@gmail.com>
2026-07-03 11:30:00 +0530

    init: setup standardized job schema column definitions

---
+++ config/schema.py
@@ -0,0 +1,16 @@
+
+JOB_COLUMNS = [
+    "job_id",
+    "job_title",
+    "company",
+    "location",
+    "experience",
+    "salary",
+    "employment_type",
+    "industry",
+    "skills",
+    "description",
+    "posted_date",
+    "source",
+    "scrape_time"
+]
